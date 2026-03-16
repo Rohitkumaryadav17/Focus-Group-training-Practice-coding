16941dc958683df9c7540b2f28f67727d79e72cc
Rohit kumar yadav <rky5825@gmail.com>
2026-03-16 07:27:55 +0530

    dsa

---
+++ string/basic.java
@@ -0,0 +1,12 @@
+package string;
+
+public class basic {
+    public static void String(String a){
+        String str="rohit";
+        System.out.println(len.str());
+
+    }
+    public static void main(String[] args) {
+        
+    }    
+}
