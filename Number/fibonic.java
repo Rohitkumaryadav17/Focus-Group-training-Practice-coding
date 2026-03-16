cb9f86af8fbfffa4d944d6803d868c480be50671
Rohit kumar yadav <rky5825@gmail.com>
2026-03-16 07:23:00 +0530

    dsa

--- Number/fibonic.java
+++ Number/fibonic.java
@@ -5,20 +5,17 @@ public class  fibonic{
     int a=0; int b=1;
     int next=0;
     for(int i=1;i<n;i++){
-        System.out.print(a+ " ");
-         next=a+b;
-        a=b;
-        b =next;
-
+    System.out.print(a+ " ");
+    next=a+b;
+    a=b;
+    b =next;
     }
 return next;
     
 }
-
-
 public static void main(String[] args) {
-    int n=25;
-    System.out.print(sumfibonic(n));
+int n=25;
+System.out.print(sumfibonic(n));
         
     }
 }
