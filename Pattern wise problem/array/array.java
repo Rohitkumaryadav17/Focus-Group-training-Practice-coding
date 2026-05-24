2ff51f4fd5c95b319761b38563105bdc86f68526
Rohit kumar yadav <rky5825@gmail.com>
2026-05-24 07:06:35 +0530

    dsa

---
+++ Pattern wise problem/array/array.java
@@ -0,0 +1,18 @@
+public class array{
+
+    public static int largest(int arr[]){
+        int max=arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+              
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int arr[]={5,3,7,8,9,-2};
+        System.out.println(largest(arr));
+        
+    }
+}
